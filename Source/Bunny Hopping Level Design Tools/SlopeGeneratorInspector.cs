f2d260629201338821209e3dcad35179f8b17f35
ORECROS\Drew <orecros@gmail.com>
2019-03-04 22:34:38 -0600

    Sorted source code into separate folders. cleaned up index.html

--- Source/Bunny Hopping Level Design Tools/SlopeGeneratorInspector.cs
+++ Source/Bunny Hopping Level Design Tools/SlopeGeneratorInspector.cs
@@ -1,3 +1,11 @@
+/*
+ * Andrew Tollett, 2018
+ * This is some example code I ripped out of an old project of mine.
+ * the code for Bezier Splines and Bezier Curves (called from here but not included here) was created
+ * by closely following this tutorial: https://catlikecoding.com/unity/tutorials/curves-and-splines/
+ * All other code in this project, including all the code in this file, was written by me.
+ */
+
 ﻿using UnityEditor;
 using UnityEngine;
 
